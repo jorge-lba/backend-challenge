479d95a2443ac329b9e504e1c4b653f26954ebb7
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-11-25 17:42:52 -0300

    feat: include remove challenge use case in module providers

--- packages/challenges/src/modules/challenges/challenges.module.ts
+++ packages/challenges/src/modules/challenges/challenges.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ChallengeRepository } from './repositories/prisma/Challenge.repository';
 import { CreateChallengeUseCase } from './useCase/createChallenge/CreateChallenge.useCase';
 import { CreateChallengeUseCaseResolver } from './useCase/createChallenge/CreateChallenge.resolver';
+import { RemoveChallengeUseCase } from './useCase/removeChallenge/RemoveChallenge.useCase';
 
 @Module({
   providers: [
     CreateChallengeUseCaseResolver,
     ChallengeRepository,
     CreateChallengeUseCase,
+    RemoveChallengeUseCase,
   ],
 })
 export class ChallengesModule {}
