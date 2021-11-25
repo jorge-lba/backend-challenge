70ee3798c155b79e391760f736556fe44c0f3a8f
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-11-25 19:54:24 -0300

    feat: include remove challenge resolver in module

--- packages/challenges/src/modules/challenges/challenges.module.ts
+++ packages/challenges/src/modules/challenges/challenges.module.ts
@@ -3,6 +3,7 @@ import { ChallengeRepository } from './repositories/prisma/Challenge.repository'
 import { CreateChallengeUseCase } from './useCase/createChallenge/CreateChallenge.useCase';
 import { CreateChallengeUseCaseResolver } from './useCase/createChallenge/CreateChallenge.resolver';
 import { RemoveChallengeUseCase } from './useCase/removeChallenge/RemoveChallenge.useCase';
+import { RemoveChallengeResolver } from './useCase/removeChallenge/RemoveChallenge.resolver';
 
 @Module({
   providers: [
