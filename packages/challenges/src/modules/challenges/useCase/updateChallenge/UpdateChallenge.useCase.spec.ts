4274b3054a14e9b40b61bfbb0a8d120070162d8a
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-11-25 22:53:25 -0300

    test: added more validations in test update

--- packages/challenges/src/modules/challenges/useCase/updateChallenge/UpdateChallenge.useCase.spec.ts
+++ packages/challenges/src/modules/challenges/useCase/updateChallenge/UpdateChallenge.useCase.spec.ts
@@ -5,6 +5,7 @@ import { UpdateChallengeUseCase } from './UpdateChallenge.useCase';
 
 describe('Update Challenge Use Case', () => {
   let updateUseCase: UpdateChallengeUseCase;
+  let challengeRepository: ChallengeRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,9 +19,36 @@ describe('Update Challenge Use Case', () => {
     }).compile();
 
     updateUseCase = module.get<UpdateChallengeUseCase>(UpdateChallengeUseCase);
+
+    challengeRepository = module.get<ChallengeRepository>(ChallengeRepository);
   });
 
   it('should be defined', () => {
     expect(updateUseCase).toBeDefined();
+  });
+
+  it('should update a challenge', async () => {
+    const challengeCreated = await challengeRepository.create({
+      title: 'Test Update Challenge',
+      description: 'should update a challenge',
+    });
+
+    const challengeData = {
+      title: 'Test Update Challenge Updated',
+      description: 'should update a challenge',
+    };
+
+    const challengeUpdated = await updateUseCase.execute(
+      challengeCreated.id,
+      challengeData,
+    );
+
+    expect(challengeUpdated.createdAt).toBeDefined();
+    expect(challengeUpdated).toEqual(
+      expect.objectContaining({
+        ...challengeData,
+        id: expect.any(String),
+      }),
+    );
   });
 });
