078013c3ac54d093ee15bac378b7fef1e554d824
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-11-25 23:47:38 -0300

    test: should be list all challenges

--- packages/challenges/src/modules/challenges/useCase/listChallenges/ListChallenges.useCase.spec.ts
+++ packages/challenges/src/modules/challenges/useCase/listChallenges/ListChallenges.useCase.spec.ts
@@ -23,4 +23,29 @@ describe('List Challenge Use Case', () => {
   it('should be defined', () => {
     expect(listUseCase).toBeDefined();
   });
+
+  it('should be list all challenges', async () => {
+    const challengeData = {
+      title: 'Remove Challenge',
+      description: 'should be removing one challenge by id',
+    };
+
+    await challengeRepository.create(challengeData);
+    await challengeRepository.create(challengeData);
+
+    const challenges = await listUseCase.execute();
+
+    const expectedChallengeData = {
+      ...challengeData,
+      id: expect.any(String),
+    };
+
+    expect(challenges.length).toBe(2);
+    expect(challenges).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining(expectedChallengeData),
+        expect.objectContaining(expectedChallengeData),
+      ]),
+    );
+  });
 });
