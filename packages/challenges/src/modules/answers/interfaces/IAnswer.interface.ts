48d21d2dafe2ff4e21028335206dbb2e66484162
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-11-27 23:51:14 -0300

    refactor: renamed interface

--- packages/challenges/src/modules/answers/interfaces/IAnswer.interface.ts
+++ packages/challenges/src/modules/answers/interfaces/IAnswer.interface.ts
@@ -1,9 +1,10 @@
 import { Status } from '../enums/Status.enum';
 
-export interface IAnswers {
+export interface IAnswer {
   id: string;
   challengeId: string;
   link: string;
   status: Status;
   grade: number;
+  createdAt: Date;
 }
